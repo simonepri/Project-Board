c1ceddf64a6574c5d8190a8d04305c3bc4fdccd3
Simone Primarosa <simone.pri@hotmail.it>
2016-02-17 15:50:20 +0100

    Unified indentation

--- app/team/team_test.js
+++ app/team/team_test.js
@@ -6,15 +6,14 @@ describe('projectBoard.team module', function() {
   beforeEach(module('projectBoard.team'));
 
   describe('TeamCtrl', function(){
+    
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      // very poor... Lord please forgive me..
+      $httpBackend.expectGET('team/team.json?nocache='+(new Date()).getTime()-1).respond([{name: 'member 1'}, {name: 'member 2'}]);
 
-
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
-        $httpBackend = _$httpBackend_;
-        // very poor... Lord please forgive me..
-        $httpBackend.expectGET('team/team.json?nocache='+(new Date()).getTime()-1).respond([{name: 'member 1'}, {name: 'member 2'}]);
-
-        scope = $rootScope.$new();
-        ctrl = $controller('TeamCtrl', {$scope:scope});
+      scope = $rootScope.$new();
+      ctrl = $controller('TeamCtrl', {$scope:scope});
     }));
     it('should create team model with two team member fetched from xhr', function() {
       expect(scope.team).toBeUndefined();
